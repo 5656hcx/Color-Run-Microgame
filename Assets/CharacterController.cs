8a2050ecd320bcd8a2ab08ef37a940149aa2dd9b
tingting-zhu <70286611+tingting-zhu@users.noreply.github.com>
2021-02-03 17:24:06 -0800

    miss jumping functionality

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public KeyCode immaterial;
+    public Rigidbody2D rb;
+    public float movespeed;
+    public int flag = 0;
+    public float jump;
+    public float horizontal;
+    public float move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movespeed = 1.5f;
+        jump = 300f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Rigidbody2D>().velocity = new Vector3(2, 0, 0);
+
+        //if (Input.GetKeyDown(immaterial))
+        //{
+        //    GetComponent<Rigidbody2D>().gravityScale = 0;
+        //    GetComponent<BoxCollider2D>().isTrigger = true;
+        //    GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, .5f);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.AddForce(new Vector2(0, jump));
+            horizontal = Input.GetAxis("Horizontal");
+            move = horizontal * movespeed;
+            rb.velocity = new Vector2(move, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (flag == 0)
+            {
+                transform.Rotate(new Vector3(0, 180, 0));
+                flag = 1;
+            }
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (flag == 1)
+            {
+                transform.Rotate(new Vector3(0, 180, 0));
+                flag = 0;
+            }
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+    }
+
+}
---
+++ Assets/GateIntersectionController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateIntersectionController : MonoBehaviour
+{
+   
+    private Vector4 originColor;
+
+    public bool IsIntersect()
+    {
+        bool isIntersect;
+        Transform moveOrthogon = GameObject.Find("character").transform;
+        Vector3 moveOrthogonPos = moveOrthogon.position;
+
+        Vector3 smallOrthogonPos = this.gameObject.transform.position;
+
+        float x1 = this.gameObject.GetComponent<Renderer>().bounds.size.x;
+        float x2 = GameObject.Find("character").GetComponent<Renderer>().bounds.size.x;
+
+        float halfSum_X = (x1 + x2) * 0.5f;
+        float distance_X = Mathf.Abs(moveOrthogonPos.x - smallOrthogonPos.x);
+
+        if (distance_X <= halfSum_X+0.03)
+        {
+            isIntersect = true;
+            //Debug.Log("is intersect");
+        }
+        else
+        {
+            isIntersect = false;
+            //Debug.Log("not intersect");
+        }
+        return isIntersect;
+    }
+
+    public void reColor(GameObject obj)
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+           
+            Vector4 color = this.gameObject.GetComponent<SpriteRenderer>().color;
+            color[3] = 1;
+            obj.GetComponent<SpriteRenderer>().color = color;
+        }
+    }
+
+    public void backToOriginColor(GameObject obj)
+    {
+
+        obj.GetComponent<SpriteRenderer>().color = originColor;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        originColor = GameObject.Find("character").GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool intersect = IsIntersect();
+        GameObject obj = GameObject.Find("character");
+        if (intersect)
+        {
+            //Debug.Log("is intersect");
+            reColor(obj);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            //Debug.Log("not intersect");
+            backToOriginColor(obj);
+        }
+    }
+
+}
