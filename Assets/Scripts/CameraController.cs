48079ef46635fd828a1870787745195429faa3ec
Chenxi Han <5656hcx@gmail.com>
2021-02-04 18:10:26 -0800

    Add scene prototype

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform focus;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 pos = focus.position;
+    	pos.z = -1;
+    	transform.position = pos;
+    }
+}
