a46acdedc0ce4721083adff092d12d5c5c8b4663
Chenxi Han <5656hcx@gmail.com>
2021-02-17 16:40:10 -0800

    Add raw first screen

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+	public TextMeshProUGUI Toast;
+	public float toastFadeRate;
+
+	public void StartGame() 
+	{
+	    SceneManager.LoadScene("MainScene");
+	}
+
+	public void NotYetImplemented()
+	{
+		Toast.gameObject.SetActive(true);
+		Toast.color = Color.white;
+	}
+
+	void Update()
+	{
+		if (Toast.gameObject.activeSelf)
+		{
+			Toast.color -= new Color(0, 0, 0, toastFadeRate * Time.deltaTime);
+			if (Toast.color.a <= 0) Toast.gameObject.SetActive(false);
+		}
+	}
+}
