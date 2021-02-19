cd7526f635302e93fdcf6c30c3e92abd17cb2df2
tingting-zhu <70286611+tingting-zhu@users.noreply.github.com>
2021-02-19 12:54:01 -0800

    add fading in and out effects between scenes

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+
+    private PlayerController player;
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+
+        if (!player.getStatus())
+        {
+            // player doesn't reach the destination
+            return;
+        }
+
+        
+        FadeToNextLevel();
+    }
+
+    public void FadeToNextLevel()
+    {
+        animator.SetTrigger("FadeOutTrigger");
+    }
+
+
+
+    public void OnFadeComplete()
+    {
+        Debug.Log("Load next level");
+        //_nextLevelIndex++;
+        //string nextLevelName = "Level" + _nextLevelIndex;
+        //SceneManager.LoadScene(nextLevelName);
+    }
+}
