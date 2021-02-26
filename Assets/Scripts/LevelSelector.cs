332f470c57aa53b7c7da604af120f82583c56d0f
Chenxi Han <5656hcx@gmail.com>
2021-02-25 17:55:33 -0800

    Implement level selection system

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelSelector : MonoBehaviour
+{
+	public string TitleBeginWith;
+	public TextMeshProUGUI Title;
+	public Sprite[] Thumbnails;
+	public Image Thumbnail;
+
+	private static int level_index = 1;
+	private const string img_name = "Level_";
+
+	void Start()
+	{
+		Title.text = TitleBeginWith + level_index;
+		Thumbnail.sprite = Thumbnails[level_index - 1];
+	}
+
+    public void Play()
+    {
+    	SceneManager.LoadScene(level_index);
+    }
+
+    public void Next()
+    {
+    	level_index++;
+    	if (level_index >= SceneManager.sceneCountInBuildSettings)
+    	{
+    		level_index = 1;
+    	}
+    	Start();
+    }
+}
