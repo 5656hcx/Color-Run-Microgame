2898f8c9684dfc1f2e426ab35a26db82d4d7eb14
Chenxi Han <5656hcx@gmail.com>
2021-02-25 19:36:37 -0800

    Update level selector per GDD

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -9,30 +9,16 @@ public class LevelSelector : MonoBehaviour
 {
 	public string TitleBeginWith;
 	public TextMeshProUGUI Title;
-	public Sprite[] Thumbnails;
-	public Image Thumbnail;
-
-	private static int level_index = 1;
-	private const string img_name = "Level_";
 
 	void Start()
 	{
-		Title.text = TitleBeginWith + level_index;
-		Thumbnail.sprite = Thumbnails[level_index - 1];
 	}
 
     public void Play()
     {
-    	SceneManager.LoadScene(level_index);
     }
 
     public void Next()
     {
-    	level_index++;
-    	if (level_index >= SceneManager.sceneCountInBuildSettings)
-    	{
-    		level_index = 1;
-    	}
-    	Start();
     }
 }
