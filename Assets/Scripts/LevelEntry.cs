02e4375397565071fb63d6810c6441f6b204399f
Chenxi Han <5656hcx@gmail.com>
2021-02-28 01:27:31 -0800

    Rewrite LevelEntry and LevelSelector

--- Assets/Scripts/LevelEntry.cs
+++ Assets/Scripts/LevelEntry.cs
@@ -8,27 +8,17 @@ public class LevelEntry : MonoBehaviour
     public Sprite img_enabled;
     public Sprite img_disabled;
     public TextMeshProUGUI text;
+    public Button btn;
 
-    private Button btn;
-    private int LevelIndex;
-
-    void Awake()
-    {
-        btn = GetComponent<Button>();
-        btn.onClick.AddListener(OnClick);
-    }
+    public delegate void OnClick(int index);
+    public OnClick clicked;
 
     public void Init(int index, bool locked)
     {
-        LevelIndex = index;
         btn.enabled = !locked;
-        text.text = LevelIndex.ToString();
+        btn.onClick.AddListener(delegate{clicked.Invoke(index);});
+        text.text = index.ToString();
         text.gameObject.SetActive(!locked);
         GetComponent<Image>().sprite = locked ? img_disabled : img_enabled;
-    }
-
-    private void OnClick()
-    {
-        SceneManager.LoadScene(LevelIndex);
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,27 +3,39 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+using datatype;
+
 public class UIController : MonoBehaviour
 {
-	public Image dialog;
 	public TextMeshProUGUI toast;
 	public GameObject buttonGroup;
 	public Animator animator;
 	public float toastFadeRate = 1;
 
+	public Transform LevelPanel;
+	public LevelEntry EntryPrefab;
+
+	private Image dialog;
 	private bool hideButtonGroup = false;
+
+	void Start()
+	{
+		InitLevelEntries();
+	}
 
 	/* Animations - Start playing the game */
 
-	public void StartPlaying()
+	public void StartPlaying(int index)
 	{
 		if (dialog != null) dialog.gameObject.SetActive(false);
+		animator.SetInteger("targetLevel", index);
 		animator.SetTrigger("StartPlaying");
+		buttonGroup.SetActive(true);
 	}
 
 	private void StartPlayingAnimEnds()
 	{
-		SceneManager.LoadScene(1);
+		SceneManager.LoadScene(animator.GetInteger("targetLevel"));
 	}
 
 	/* Animations - Show dialogs if any */
@@ -56,9 +68,9 @@ public class UIController : MonoBehaviour
 		else buttonGroup.SetActive(!hideButtonGroup);
 	}
 
+	// Ugly solution
 	public void SetHideButtonGroup(bool flag)
 	{
-		//buttonGroup.SetActive(!flag);
 		hideButtonGroup = flag;
 	}
 
@@ -76,6 +88,41 @@ public class UIController : MonoBehaviour
 		{
 			toast.color -= new Color(0, 0, 0, toastFadeRate * Time.deltaTime);
 			if (toast.color.a <= 0) toast.gameObject.SetActive(false);
+		}
+	}
+
+	private void InitLevelEntries()
+	{
+		Level[] progress = XMLHelper.Load<Level>(Level.path);
+		if (progress.Length == 0)
+		{
+			progress = new Level[SceneManager.sceneCountInBuildSettings - 1];
+			for (int i=0; i<progress.Length; i++)
+			{
+				progress[i] = new Level(i+1);
+			}
+			progress[0].state = true;
+			XMLHelper.Save<Level>(ref progress, Level.path);
+		}
+
+		if (progress.Length < SceneManager.sceneCountInBuildSettings - 1)
+		{
+			Level[] tmp = new Level[SceneManager.sceneCountInBuildSettings - 1];
+			for (int i=0; i<tmp.Length; i++)
+			{
+				tmp[i] = i < progress.Length ? progress[i] : new Level(i+1);
+			}
+			progress = tmp;
+			XMLHelper.Save<Level>(ref progress, Level.path);
+		}
+
+		for (int i=0; i<SceneManager.sceneCountInBuildSettings - 1; i++)
+		{
+			LevelEntry entry = Instantiate(EntryPrefab);
+			entry.clicked = new LevelEntry.OnClick(StartPlaying);
+			entry.Init(progress[i].index, !progress[i].state);
+			entry.transform.localScale = new Vector3(1, 1, 0);
+			entry.transform.SetParent(LevelPanel);
 		}
 	}
 }
