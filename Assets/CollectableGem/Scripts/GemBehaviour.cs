ebc7ab44f74505bf5d4684a37bb0075847267277
Chenxi Han <5656hcx@gmail.com>
2021-03-15 00:19:30 -0700

    Add assets for Gem by Zhuofan

---
+++ Assets/CollectableGem/Scripts/GemBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemBehaviour : MonoBehaviour
+{
+	[Header("References")]
+	public GameObject gemVisuals;
+	public GameObject collectedParticleSystem;
+	public CircleCollider2D gemCollider2D;
+
+	private float durationOfCollectedParticleSystem;
+
+
+	void Start()
+	{
+		durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D theCollider)
+	{
+		if (theCollider.CompareTag ("Player")) {
+			GemCollected ();
+		}
+	}
+
+	void GemCollected()
+	{
+		gemCollider2D.enabled = false;
+		gemVisuals.SetActive (false);
+		collectedParticleSystem.SetActive (true);
+		Invoke ("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+		GemsUI.CurrentGemQuantity += 1;
+
+	}
+
+	void DeactivateGemGameObject()
+	{
+		gameObject.SetActive (false);
+	}
+}
