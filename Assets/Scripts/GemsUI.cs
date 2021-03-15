90d2d520bf09479c4d9142cdbfb9433ac4135288
Chenxi Han <5656hcx@gmail.com>
2021-03-15 00:20:57 -0700

    Add functions for collect & purchase by Zhuofan

---
+++ Assets/Scripts/GemsUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GemsUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text gemQuantity;
+    public static int CurrentGemQuantity;
+    public PlayerStatistics localPlayerData;
+    void Start()
+    {
+        localPlayerData = GlobalControl.Instance.savedPlayerData;
+        CurrentGemQuantity = localPlayerData.Gems;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gemQuantity.text = CurrentGemQuantity.ToString();
+    }
+}
