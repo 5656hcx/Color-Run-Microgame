d25f33836845616c18f5c38c77186dc9434b2d44
tingting-zhu <70286611+tingting-zhu@users.noreply.github.com>
2021-03-25 18:39:44 -0700

    add a moving platform

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlatformController : PhysicsObject
+{
+
+    public float startPos;
+    public float endPos;
+    public bool moveUp;
+    public bool moveVertically;
+
+    public float verticalSpeed = 1.0f;
+
+    void Awake()
+    {
+        
+    }
+     
+    protected override void ComputeVelocity()
+    {
+        if (transform.position.y > endPos)
+        {
+            moveUp = false;
+        }
+
+        if (transform.position.y < startPos)
+        {
+            moveUp = true;
+        }
+
+
+        if (moveUp)
+        {
+            velocity.y = verticalSpeed;
+        }
+        else
+        {
+            velocity.y = -verticalSpeed;
+        }
+
+
+    }
+}
